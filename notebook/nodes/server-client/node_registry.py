1146f2732cde933a3eb6284f6fce0c7d3e2ab62c
NQQT <devilz.angelz@gmail.com>
2025-08-04 12:04:44 +1000

    Updated Video Image Save Node

--- notebook/nodes/server-client/node_registry.py
+++ notebook/nodes/server-client/node_registry.py
@@ -1,13 +1,20 @@
 from .nodes.ClientImageSaveNode import ImageSaveNode
+from .nodes.ClientMp4VideoSaveNode import Mp4VideoSaveNode
 from .nodes.ClientVideoSaveNode import VideoSaveNode
 
 # Node Registration for ComfyUI
 NODE_CLASS_MAPPINGS = {
     "ClientImageSaveNode": ImageSaveNode,
-    "ClientVideoSaveNode": VideoSaveNode
+    "ClientVideoSaveNode": VideoSaveNode,
+
+    # Experimental
+    "ClientMp4VideoSaveNode": Mp4VideoSaveNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ClientImageSaveNode": "Client Image Save",
-    "ClientVideoSaveNode": "Client Video Save"
+    "ClientImageSaveNode": "Client Image Download",
+    "ClientVideoSaveNode": "Client Video Download",
+
+    # Experimental
+    "ClientMp4VideoSaveNode": "Client Mp4 Video Download",
 }
