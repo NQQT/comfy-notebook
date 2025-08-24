88cf06bcfacf812cf72d4eafa1930664a9342d17
NQQT <devilz.angelz@gmail.com>
2025-08-25 00:47:47 +1000

    Added Utility Functions

--- nofusion/src/nofusion/source/library/lora.py
+++ nofusion/src/nofusion/source/library/lora.py
@@ -1,3 +1,4 @@
+from ..civitai import civitai_fetch_loras
 from ...comfy import fetch_asset_loras
 
 
@@ -10,3 +11,20 @@ def source_wan_i2v_lightning_loras():
         f"{lora_location}/Wan2.2-Lightning_I2V-A14B-4steps-lora_HIGH_fp16.safetensors")
     fetch_asset_loras(
         f"{lora_location}/Wan2.2-Lightning_I2V-A14B-4steps-lora_LOW_fp16.safetensors")
+
+
+def source_wan_i2v_nsfw_loras():
+    # Cock Play
+    civitai_fetch_loras("CockPlay_i2v_HighNoise", "2087173")
+    civitai_fetch_loras("CockPlay_i2v_LowNoise", "2087124")
+
+    # General NSFW
+    civitai_fetch_loras("GeneralNSFW_i2v_HighNoise", "2073605")
+    civitai_fetch_loras("GeneralNSFW_i2v_LowNoise", "2083303")
+
+    # Oral Insertion
+    civitai_fetch_loras("OralInsertion_i2v", "2121297")
+
+    # Anime cumshot Aesthetics
+    civitai_fetch_loras("AnimeCumshotAesthetics_i2v_HighNoise", "2116008")
+    civitai_fetch_loras("AnimeCumshotAesthetics_i2v_LowNoise", "2116027")
