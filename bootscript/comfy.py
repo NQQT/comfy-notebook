543bd5ee2ad8b9e8247e88161555e6149ba8be65
NQQT <devilz.angelz@gmail.com>
2025-09-05 22:32:47 +1000

    Update Configuration file

--- bootscript/comfy.py
+++ bootscript/comfy.py
@@ -1,11 +1,5 @@
 import os
 
-# Git Repo Name
-git_repo_name = "comfy-notebook"
-
-os.system(f"pip install --upgrade {git_repo_name}/repository")
-os.system(f"pip install --upgrade {git_repo_name}/nofusion")
-
 from nofusion.comfy import install_comfy_ui
 from nofusion.config import variables
 
@@ -14,5 +8,5 @@ variables({
     "root": os.getcwd()
 })
 
-# Installing ComfyUI
+# Installing ComfyUI and all dependencies
 install_comfy_ui()
