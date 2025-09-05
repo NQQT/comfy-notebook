9e1c024963340b03a85d97fc404ffba2a1ac38b7
NQQT <devilz.angelz@gmail.com>
2025-09-06 00:53:35 +1000

    Update Configuration file

--- bootscript/comfy.py
+++ bootscript/comfy.py
@@ -1,7 +1,5 @@
 import os
 
-from IPython.core.display_functions import clear_output
-
 from nofusion.comfy import install_comfy_ui
 from nofusion.config import variables
 from nofusion.source import source_model_pony_cyber_realistic, source_model_wan_i2v, source_wan_i2v_lightning_loras, \
@@ -22,4 +20,3 @@ source_model_pony_cyber_realistic()
 source_model_wan_i2v("14B_Q5_K_M")
 source_wan_i2v_lightning_loras()
 source_wan_i2v_nsfw_loras()
-clear_output()
