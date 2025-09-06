7ee1579b3a9385d70dc32dc897a4cce54dd2ed11
NQQT <devilz.angelz@gmail.com>
2025-09-07 00:31:06 +1000

    Update Configuration file

--- bootscript/load/wan_t2v.py
+++ bootscript/load/wan_t2v.py
@@ -1,7 +1,12 @@
+import os
+
 from nofusion.source import source_model_wan_t2v, source_wan_t2v_lightning_loras, source_wan_t2v_nsfw_loras
 
+package_dir = os.path.dirname(__file__)
+# Setting up the minimum requirements
+common_script = os.path.join(package_dir, '../common.py')
 # Running common file first
-with open("../common.py") as file:
+with open(common_script, "r") as file:
     exec(file.read())
 
 # Loading T2V Model
