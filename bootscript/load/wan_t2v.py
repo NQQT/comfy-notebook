3094a49ac37e59d39f670d4a6e768eca15b996ec
NQQT <devilz.angelz@gmail.com>
2025-09-07 23:29:45 +1000

    Update Configuration file

--- bootscript/load/wan_t2v.py
+++ bootscript/load/wan_t2v.py
@@ -2,6 +2,7 @@ import os
 
 from nofusion.source import source_model_wan_t2v, source_wan_t2v_lightning_loras, source_wan_t2v_nsfw_loras
 
+# Standard Comfy File Setup
 package_dir = os.path.dirname(__file__)
 # Setting up the minimum requirements
 common_script = os.path.join(package_dir, '../common.py')
