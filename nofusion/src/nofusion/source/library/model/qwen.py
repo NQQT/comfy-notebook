059640c8baaade3696ef842a219254f523ba528c
NQQT <devilz.angelz@gmail.com>
2025-09-10 16:06:19 +1000

    Updated Qwen Configuration

--- nofusion/src/nofusion/source/library/model/qwen.py
+++ nofusion/src/nofusion/source/library/model/qwen.py
@@ -6,8 +6,8 @@ from noobish.core import string_switch
 def source_model_qwen_edit(model_size="Q8_0"):
     model_source = "https://huggingface.co/QuantStack/Qwen-Image-Edit-GGUF/resolve/main"
     encoder_source = "https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/text_encoders"
-    vae_source = "https://huggingface.co/QuantStack/Qwen-Image-GGUF/resolve/main/VAE/"
-
+    vae_source = "https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/vae/"
+    
     # Downloading Qwen Model
     fetch_asset_diffusion_models(f"{model_source}/Qwen_Image_Edit-{model_size}.gguf")
 
@@ -21,4 +21,4 @@ def source_model_qwen_edit(model_size="Q8_0"):
     fetch_asset_text_encoders(f"{encoder_source}/qwen_2.5_vl_7b{encoder_size}.safetensors")
 
     # Vae
-    fetch_asset_vae(f"{vae_source}/Qwen_Image-VAE.safetensors")
+    fetch_asset_vae(f"{vae_source}/qwen_image_vae.safetensors")
