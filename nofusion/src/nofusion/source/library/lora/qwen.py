1cbb3216930fd7a7e136c68aec4546f6a5db2c6f
NQQT <devilz.angelz@gmail.com>
2025-09-10 14:40:02 +1000

    Updated Qwen Configuration

--- nofusion/src/nofusion/source/library/lora/qwen.py
+++ nofusion/src/nofusion/source/library/lora/qwen.py
@@ -1,11 +1,13 @@
-from ..model.qwen import source_model_qwen_edit
 from ...civitai import civitai_fetch_loras
 
 
 # Installing useful loras
 def source_lora_qwen_edit_useful():
-    # Need to loading standard qwen model first
-    source_model_qwen_edit()
-    
     # Anime to Realism: https://civitai.com/models/1934100
-    civitai_fetch_loras("anime_2_realism", "2189067")
+    civitai_fetch_loras("qwen_anime_2_realism", "2189067")
+
+    # Outfit extractor: https://civitai.com/models/1940557?modelVersionId=2196307
+    civitai_fetch_loras("qwen_outfit_extractor", "2196307")
+
+    # Try on outfit: https://civitai.com/models/1940532?modelVersionId=2196278
+    civitai_fetch_loras("qwen_try_on", "2196278")
