ee91fe5caaebcd0851e0e4691703c0df558c1ed2
NQQT <devilz.angelz@gmail.com>
2025-09-24 16:15:22 +1000

    Updated Qwen Configuration

--- bootscript/load/pony.py
+++ bootscript/load/pony.py
@@ -1,6 +1,6 @@
 import os
 
-from nofusion.source import source_model_pony_cyber_realistic, civitai_fetch_loras
+from nofusion.source import source_model_pony_cyber_realistic, civitai_fetch_loras, civitai_fetch_checkpoints
 
 # Standard Comfy File Setup
 package_dir = os.path.dirname(__file__)
@@ -12,6 +12,12 @@ with open(common_script, "r") as file:
 
 # Setting up Cyber realistic
 source_model_pony_cyber_realistic()
+
+# https://civitai.com/models/1559047?modelVersionId=1971591
+civitai_fetch_checkpoints("LucentXLPonyByKlaabu", "1971591")
+
+# https://civitai.com/models/1115064/pony-realism-slider?modelVersionId=1253021
+civitai_fetch_loras("pony_realism", "1253021")
 
 civitai_fetch_loras("pony_beauty_slider", "518458")
 # https://civitai.com/models/341353/expressiveh-hentai-lora-style
