6fdba9960abab53058a8f79d35d4d4952179fa9e
NQQT <devilz.angelz@gmail.com>
2026-02-03 23:30:54 +1100

    Updated Wan Configuration

--- nofusion/src/nofusion/source/library/model/__init__.py
+++ nofusion/src/nofusion/source/library/model/__init__.py
@@ -2,3 +2,4 @@ from .pony import *
 from .qwen import *
 from .supir import *
 from .wan import *
+from .zimage import *
---
+++ nofusion/src/nofusion/source/library/model/zimage.py
@@ -0,0 +1,15 @@
+# For Setting up Cyber Realistic
+from ....comfy import fetch_asset_vae, fetch_asset_text_encoders
+from ....source import civitai_fetch_checkpoints
+
+
+def source_model_zimage_moody_mix():
+    # Moody Mix
+    civitai_fetch_checkpoints("ZImageMoodyMix", "2633363")
+
+    # Text-encoder
+    fetch_asset_text_encoders(
+        "https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/text_encoders/qwen_3_4b.safetensors")
+
+    # AE
+    fetch_asset_vae("https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/vae/ae.safetensors")
